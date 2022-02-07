e52d700e5dfd82d6773d33f6b3e0d46b186a36bd
decca-dev <medcherbani@gmail.com>
2022-02-07 19:03:46 +0100

    info command

---
+++ src/Commands/utilities/Info.ts
@@ -0,0 +1,188 @@
+import { Command } from "../../Structures/Command.js";
+import {
+  GuildBasedChannel,
+  GuildEmoji,
+  GuildMember,
+  Role,
+  Collection,
+} from "discord.js";
+import pretty from "pretty-ms";
+
+interface GuildData {
+  name: string;
+  createdAt: number;
+  icon: string;
+  owner: string;
+  memberCount: number;
+  channels: Collection<string, GuildBasedChannel>;
+  roles: Collection<string, Role>;
+  emojis: Collection<string, GuildEmoji>;
+  members: Collection<string, GuildMember>;
+}
+
+interface BotData {
+  name: string;
+  createdAt: number;
+  avatar: string;
+  owner: string;
+  users: number;
+  channels: number;
+  uptime: string;
+  platform: string;
+}
+
+export default new Command({
+  name: "info",
+  description: "Returns general info about the server, users, bot, etc...",
+  usage: "info <guild | bot | user @User/user_id>",
+  cooldown: 10,
+  ownerOnly: false,
+  adminOnly: false,
+  permissions: [],
+  category: "utilities",
+  run: async (client, message, args) => {
+    const validArgs = ["guild", "bot", "user"];
+    if (!args[0]) {
+      message.reply(
+        `**Invalid argument!** Please use one of these arguments next time:\n${validArgs
+          .map((arg) => `${arg}`)
+          .join(", ")}`
+      );
+      return;
+    }
+    switch (args[0]) {
+      case "guild":
+        const guild: GuildData = {
+          name: message.guild?.name!,
+          createdAt: message.guild?.createdTimestamp!,
+          icon: message.guild?.iconURL({ dynamic: true })!,
+          owner: `<@!${message.guild?.ownerId}>`,
+          memberCount: message.guild?.memberCount!,
+          channels: message.guild?.channels.cache!,
+          roles: message.guild?.roles.cache.filter(
+            (role) => role.name !== "@everyone"
+          )!,
+          emojis: message.guild?.emojis.cache!,
+          members: message.guild?.members.cache!,
+        };
+
+        message.channel.send({
+          embeds: [
+            {
+              title: guild.name,
+              color: "RED",
+              //@ts-ignore
+              thumbnail: guild.icon,
+              fields: [
+                { name: "Guild Owner", value: guild.owner, inline: false },
+                {
+                  name: "Creation Date",
+                  value: new Date(guild.createdAt).toLocaleDateString("en-US"),
+                  inline: false,
+                },
+                {
+                  name: "Total Members",
+                  value: `${guild.memberCount}`,
+                  inline: true,
+                },
+                {
+                  name: "Users",
+                  value: `${guild.members.filter((m) => !m.user.bot).size}`,
+                  inline: true,
+                },
+                {
+                  name: "Bots",
+                  value: `${guild.members.filter((m) => m.user.bot).size}`,
+                  inline: true,
+                },
+                {
+                  name: "Channels",
+                  value: `${guild.channels.size}`,
+                  inline: true,
+                },
+                {
+                  name: "Text",
+                  value: `${
+                    guild.channels.filter((ch) => ch.type === "GUILD_TEXT").size
+                  }`,
+                  inline: true,
+                },
+                {
+                  name: "Voice",
+                  value: `${
+                    guild.channels.filter((ch) => ch.type === "GUILD_VOICE")
+                      .size
+                  }`,
+                  inline: true,
+                },
+                {
+                  name: "Roles",
+                  value: guild.roles.map((role) => `<@&${role.id}>`).join(", "),
+                  inline: false,
+                },
+                {
+                  name: "Emojis",
+                  value:
+                    guild.emojis.size > 0
+                      ? guild.emojis
+                          .map((emoji) => `<${emoji.name}:${emoji.id}>`)
+                          .join(" ")
+                      : "None",
+                  inline: false,
+                },
+              ],
+            },
+          ],
+        });
+        break;
+      case "bot":
+        const bot: BotData = {
+          name: client.user?.username!,
+          createdAt: client.user?.createdTimestamp!,
+          avatar: client.user?.avatarURL({ dynamic: false })!,
+          owner: `<@!${process.env.OWNER_ID}>`,
+          users: client.users.cache.size,
+          channels: client.channels.cache.size,
+          uptime: pretty(client.uptime!),
+          platform: process.platform,
+        };
+
+        message.channel.send({
+          embeds: [
+            {
+              title: bot.name,
+              color: "RED",
+              //@ts-ignore
+              thumbnail: bot.avatar,
+              fields: [
+                {
+                  name: "Created at",
+                  value: new Date(bot.createdAt).toLocaleDateString("en-US"),
+                  inline: false,
+                },
+                { name: "Owner", value: bot.owner, inline: false },
+                { name: "User cache", value: `${bot.users}`, inline: true },
+                {
+                  name: "Channel cache",
+                  value: `${bot.channels}`,
+                  inline: true,
+                },
+                { name: "Uptime", value: `${bot.uptime}`, inline: false },
+                { name: "Plotform", value: `${bot.platform}`, inline: false },
+              ],
+            },
+          ],
+        });
+        break;
+      case "user":
+        break;
+      default:
+        message.reply(
+          `**Invalid argument!** Please use one of these arguments next time:\n${validArgs
+            .map((arg) => `${arg}`)
+            .join(", ")}`
+        );
+        break;
+    }
+  },
+});
